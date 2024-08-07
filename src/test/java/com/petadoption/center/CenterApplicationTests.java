887c0bc6a970a97d91bb2675a2d037da84b5396c
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-08-07 14:52:12 +0100

    Initial commit

---
+++ src/test/java/com/petadoption/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petadoption.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
