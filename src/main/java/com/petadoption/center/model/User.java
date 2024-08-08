cc27d85bdfc7e3f9694109bcaf5a93e76fb52083
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-08-08 10:58:34 +0100

    Added Lombok to project. Added more properties to User class

--- src/main/java/com/petadoption/center/model/User.java
+++ src/main/java/com/petadoption/center/model/User.java
@@ -1,10 +1,14 @@
 package com.petadoption.center.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "Users")
 public class User {
 
@@ -22,59 +26,9 @@ public class User {
 
     private String address;
 
+    private List<Pet> favoritePets;
 
-    public User() {
-    }
+    private List<Pet> adoptedPets;
 
-    public User(String firstName, String lastName, String email, LocalDate dateOfBirth, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    private List<AdoptionForm> adoptionForms;
 }
