4b7941c5ca8617ecf1e1506f3b613679bf8c284c
Fguedes10 <fadg100@gmail.com>
2024-08-26 14:55:08 +0100

    finish addNewOrganization and UpdateOrganization

--- src/main/java/com/petadoption/center/service/OrganizationService.java
+++ src/main/java/com/petadoption/center/service/OrganizationService.java
@@ -4,6 +4,8 @@ import com.petadoption.center.dto.organization.OrganizationCreateDto;
 import com.petadoption.center.dto.organization.OrganizationGetDto;
 import com.petadoption.center.dto.organization.OrganizationUpdateDto;
 import com.petadoption.center.exception.organization.*;
+import com.petadoption.center.exception.user.UserEmailDuplicateException;
+import com.petadoption.center.exception.user.UserPhoneNumberDuplicateException;
 
 import java.util.List;
 
@@ -15,5 +17,5 @@ public interface OrganizationService {
 
     OrganizationGetDto addNewOrganization(OrganizationCreateDto organization) throws OrganizationDuplicateSocialMediaException, OrganizationDuplicatePhoneNumberException, OrganizationDuplicateAddressException, OrganizationDuplicateWebsiteException, OrganizationDuplicateEmailException;
 
-    OrganizationGetDto updateOrganization(Long id, OrganizationUpdateDto organization) throws OrganizationNotFoundException, OrganizationDuplicateSocialMediaException, OrganizationDuplicatePhoneNumberException, OrganizationDuplicateAddressException, OrganizationDuplicateWebsiteException, OrganizationDuplicateEmailException;
+    OrganizationGetDto updateOrganization(Long id, OrganizationUpdateDto organization) throws OrganizationNotFoundException, OrganizationDuplicateSocialMediaException, OrganizationDuplicatePhoneNumberException, OrganizationDuplicateAddressException, OrganizationDuplicateWebsiteException, OrganizationDuplicateEmailException, UserEmailDuplicateException, UserPhoneNumberDuplicateException;
 }
