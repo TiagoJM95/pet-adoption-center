f5f2b569056ba2c80fad97609a365a2c92f70792
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-09-11 18:03:39 +0100

    Added unique constraints to certain Organization properties. Encapsulation done properly. Only Service has access to repository and entity. Refactor due to change in updateFields method signature. Removed all dupe checks, to allow DB to manage data inconsistency.

--- src/main/java/com/petadoption/center/controller/OrganizationController.java
+++ src/main/java/com/petadoption/center/controller/OrganizationController.java
@@ -33,13 +33,13 @@ public class OrganizationController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<OrgGetDto> addNewOrganization(@Valid @RequestBody OrgCreateDto organization) throws OrgDuplicateSocialMediaException, OrgDuplicatePhoneNumberException, OrgDuplicateAddressException, OrgDuplicateWebsiteException, OrgDuplicateEmailException {
+    public ResponseEntity<OrgGetDto> addNewOrganization(@Valid @RequestBody OrgCreateDto organization) throws OrganizationDuplicateException {
         return new ResponseEntity<>(organizationService.addNewOrganization(organization), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<OrgGetDto> updateOrganization(@PathVariable ("id") Long id,
-                                                        @Valid @RequestBody OrgUpdateDto organization) throws OrgDuplicateSocialMediaException, OrgNotFoundException, OrgDuplicatePhoneNumberException, OrgDuplicateAddressException, OrgDuplicateWebsiteException, OrgDuplicateEmailException {
+                                                        @Valid @RequestBody OrgUpdateDto organization) throws OrgNotFoundException, OrganizationDuplicateException {
         return new ResponseEntity<>(organizationService.updateOrganization(id, organization), HttpStatus.OK);
     }
 
--- src/main/java/com/petadoption/center/service/OrganizationService.java
+++ src/main/java/com/petadoption/center/service/OrganizationService.java
@@ -10,15 +10,13 @@ import java.util.List;
 
 public interface OrganizationService {
 
-    Organization findOrgById(Long id) throws OrgNotFoundException;
-
     List<OrgGetDto> getAllOrganizations(int page, int size, String sortBy);
 
     OrgGetDto getOrganizationById(Long id) throws OrgNotFoundException;
 
-    OrgGetDto addNewOrganization(OrgCreateDto organization) throws OrgDuplicateSocialMediaException, OrgDuplicatePhoneNumberException, OrgDuplicateAddressException, OrgDuplicateWebsiteException, OrgDuplicateEmailException;
+    OrgGetDto addNewOrganization(OrgCreateDto organization) throws OrganizationDuplicateException;
 
-    OrgGetDto updateOrganization(Long id, OrgUpdateDto organization) throws OrgNotFoundException, OrgDuplicateSocialMediaException, OrgDuplicatePhoneNumberException, OrgDuplicateAddressException, OrgDuplicateWebsiteException, OrgDuplicateEmailException;
+    OrgGetDto updateOrganization(Long id, OrgUpdateDto organization) throws OrgNotFoundException, OrganizationDuplicateException;
 
     String deleteOrganization(Long id) throws OrgNotFoundException;
 }
