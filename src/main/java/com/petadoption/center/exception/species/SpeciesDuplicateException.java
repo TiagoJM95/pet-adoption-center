5be459a944e4dbb4f6eca058482b1a01b4a5aa1d
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-09-11 17:57:02 +0100

    Refactoring related to exceptions, converter method's name changes and updateFields new method params. Refactored all variables of type DTO to "dto". Encapsulation done properly now. Only Service has access to repository and proper entities. Refactor SpeciesNameDuplicateException.java to SpeciesDuplicateException

---
+++ src/main/java/com/petadoption/center/exception/species/SpeciesDuplicateException.java
@@ -0,0 +1,7 @@
+package com.petadoption.center.exception.species;
+
+public class SpeciesDuplicateException extends SpeciesException {
+    public SpeciesDuplicateException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/petadoption/center/exception/species/SpeciesNameDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package com.petadoption.center.exception.species;
-
-public class SpeciesNameDuplicateException extends SpeciesException {
-    public SpeciesNameDuplicateException(String name) {
-        super("Species with name " + name + " already exists");
-    }
-}
