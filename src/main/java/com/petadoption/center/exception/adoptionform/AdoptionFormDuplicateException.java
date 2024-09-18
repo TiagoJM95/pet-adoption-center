d9cb0a319a2c7c0e0f711c2093985b0325a6fd03
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-09-18 11:55:14 +0100

    added AdoptionForm exceptions

---
+++ src/main/java/com/petadoption/center/exception/adoptionform/AdoptionFormDuplicateException.java
@@ -0,0 +1,7 @@
+package com.petadoption.center.exception.adoptionform;
+
+public class AdoptionFormDuplicateException extends Exception {
+  public AdoptionFormDuplicateException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/petadoption/center/exception/adoptionform/AdoptionFormException.java
@@ -0,0 +1,7 @@
+package com.petadoption.center.exception.adoptionform;
+
+public class AdoptionFormException extends Exception {
+    public AdoptionFormException(String message) {
+        super(message);
+    }
+}
