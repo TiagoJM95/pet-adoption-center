7a7a5193c55a672b3e5223142085b041a9153b3d
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-09-19 10:26:24 +0100

    Deleted obsolete files

--- src/main/java/com/petadoption/center/dto/breed/BreedGetDto.java
+++ src/main/java/com/petadoption/center/dto/breed/BreedGetDto.java
@@ -1,7 +1,9 @@
 package com.petadoption.center.dto.breed;
 
 import com.petadoption.center.dto.species.SpeciesGetDto;
+import lombok.Builder;
 
+@Builder
 public record BreedGetDto(
         String id,
         String name,
