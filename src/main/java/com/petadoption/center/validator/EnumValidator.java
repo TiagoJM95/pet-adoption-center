8619b2da8a780e900788c501bc719b5d890b88e6
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-10-03 12:02:08 +0100

    refactored the code to allow for dynamic error message handling depending on the class, updated Messages

--- src/main/java/com/petadoption/center/validator/EnumValidator.java
+++ src/main/java/com/petadoption/center/validator/EnumValidator.java
@@ -1,4 +1,20 @@
 package com.petadoption.center.validator;
 
-public interface EnumValidator {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = EnumValidatorConstraint.class)
+public @interface EnumValidator {
+
+    Class<? extends Enum<?>> enumClass();
+    String message() default "Invalid value";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/petadoption/center/validator/EnumValidatorConstraint.java
+++ src/main/java/com/petadoption/center/validator/EnumValidatorConstraint.java
@@ -1,4 +1,44 @@
 package com.petadoption.center.validator;
 
-public class EnumValidatorConstraint {
+import com.petadoption.center.util.Messages;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.text.MessageFormat;
+
+import static com.petadoption.center.util.Messages.REQUIRED_FIELD;
+
+public class EnumValidatorConstraint implements ConstraintValidator<EnumValidator, String> {
+
+    private Class<? extends Enum<?>> enumClass;
+    private String messageTemplate;
+
+    @Override
+    public void initialize(EnumValidator constraintAnnotation) {
+        this.enumClass = constraintAnnotation.enumClass();
+        this.messageTemplate = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(REQUIRED_FIELD)
+                    .addConstraintViolation();
+            return false;
+        }
+
+        String editedValue = value.trim().replace(" ", "_").toUpperCase();
+        Object[] enumValues = enumClass.getEnumConstants();
+        for (Object enumValue : enumValues) {
+            if (editedValue.equals(enumValue.toString())) return true;
+        }
+
+        context.disableDefaultConstraintViolation();
+        String formattedMessage = MessageFormat.format(messageTemplate, value);
+        context.buildConstraintViolationWithTemplate(formattedMessage)
+                .addConstraintViolation();
+
+        return false;
+    }
 }
