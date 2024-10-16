7216d35f56746c7a6c6831f7d98a472dabc4fac1
fguedes10 <fadg100@gmail.com>
2024-10-16 17:46:43 +0100

    update breed test to persist breed and compare dtos

--- src/test/java/com/petadoption/center/controller/integration/BreedControllerTest.java
+++ src/test/java/com/petadoption/center/controller/integration/BreedControllerTest.java
@@ -13,6 +13,7 @@ import static com.petadoption.center.testUtils.TestDtoFactory.breedUpdateDto;
 import static com.petadoption.center.testUtils.TestDtoFactory.primaryBreedCreateDto;
 import static com.petadoption.center.util.Messages.BREED_DELETE_MESSAGE;
 import static java.lang.String.format;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,6 +24,7 @@ public class BreedControllerTest extends TestContainerConfig {
     private BreedGetDto breedGetDto;
     private BreedCreateDto breedCreateDto;
     private BreedUpdateDto breedUpdateDto;
+    private String breedId;
 
 
     @BeforeAll
@@ -41,30 +43,36 @@ public class BreedControllerTest extends TestContainerConfig {
         speciesRepository.deleteAll();
     }
 
-    private void persistBreed() throws Exception {
+    private BreedGetDto persistBreed() throws Exception {
 
         var result = mockMvc.perform(post("/api/v1/breed/")
                         .content(objectMapper.writeValueAsString(breedCreateDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name", is(breedCreateDto.name())))
                 .andReturn();
 
-        breedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+        BreedGetDto breedCreatedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+        breedId = breedCreatedGetDto.id();
+        return breedCreatedGetDto;
     }
 
     @Test
     @DisplayName("Test create breed is working correctly")
     void createBreed() throws Exception {
 
-        var result = mockMvc.perform(post("/api/v1/breed/")
-                        .content(objectMapper.writeValueAsString(breedCreateDto))
+        BreedGetDto breedCreatedGetDto = persistBreed();
+
+        var result = mockMvc.perform(get("/api/v1/breed/id/{id}", breedId)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name", is(breedCreateDto.name())))
+                .andExpect(status().isOk())
                 .andReturn();
 
-        breedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+        BreedGetDto breedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+
+        assertThat(breedGetDto)
+                .usingRecursiveComparison()
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(breedCreatedGetDto);
     }
 
 
@@ -72,18 +80,19 @@ public class BreedControllerTest extends TestContainerConfig {
     @DisplayName("Test if get all breeds works correctly")
     void getAll() throws Exception {
 
-        persistBreed();
+        BreedGetDto breedCreatedGetDto = persistBreed();
 
-        List<BreedGetDto> breedGetDtos = List.of(breedGetDto);
-        String expectedJson = objectMapper.writeValueAsString(breedGetDtos);
-
-        mockMvc.perform(get("/api/v1/breed/")
-                        .param("page", "0")
-                        .param("size", "5")
-                        .param("sort", "id")
+        var result = mockMvc.perform(get("/api/v1/breed/")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", is(1)));
+                .andReturn();
+
+        BreedGetDto[] breedGetDtoArray = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto[].class);
+        assertThat(breedGetDtoArray).hasSize(1);
+        assertThat(breedGetDtoArray[0])
+                .usingRecursiveComparison()
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(breedCreatedGetDto);
     }
 
 
@@ -91,13 +100,18 @@ public class BreedControllerTest extends TestContainerConfig {
     @DisplayName("Test if get breed by id works correctly")
     void getById() throws Exception {
 
-        persistBreed();
+        BreedGetDto breedCreatedGetDto = persistBreed();
 
-        String expectedJson = objectMapper.writeValueAsString(breedGetDto);
-
-        mockMvc.perform(get("/api/v1/breed/id/{id}", breedGetDto.id())
+        var result = mockMvc.perform(get("/api/v1/breed/id/{id}", breedId)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BreedGetDto breedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+        assertThat(breedGetDto)
+                .usingRecursiveComparison()
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(breedCreatedGetDto);
     }
 
 
@@ -115,13 +129,26 @@ public class BreedControllerTest extends TestContainerConfig {
     @DisplayName("Test if update breed works correctly")
     void update() throws Exception {
 
+        BreedGetDto expectedUpdatedBreedGetDto = BreedGetDto.builder()
+                .name(breedUpdateDto.name())
+                .build();
+
         persistBreed();
 
-        mockMvc.perform(put("/api/v1/breed/update/{id}",breedGetDto.id())
+        var result = mockMvc.perform(put("/api/v1/breed/update/{id}",breedId)
                         .content(objectMapper.writeValueAsString(breedUpdateDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(breedUpdateDto.name())));
+                .andReturn();
+
+        BreedGetDto actualUpdatedBreedGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), BreedGetDto.class);
+        assertThat(actualUpdatedBreedGetDto)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "speciesDto")
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(expectedUpdatedBreedGetDto);
+
+
     }
 
     @Test
@@ -140,10 +167,10 @@ public class BreedControllerTest extends TestContainerConfig {
 
         persistBreed();
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/breed/delete/{id}",breedGetDto.id())
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/breed/delete/{id}",breedId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(format(BREED_DELETE_MESSAGE, breedGetDto.id())));
+                .andExpect(content().string(format(BREED_DELETE_MESSAGE, breedId)));
     }
 
     @Test
