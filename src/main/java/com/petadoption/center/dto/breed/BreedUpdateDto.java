0109bc6522f916a5f9076fd413367264a9d26b0f
fguedes10 <fadg100@gmail.com>
2024-10-17 15:09:28 +0100

    add space in regex of name in BreedUpdateDto
    refactor all name tests and changed urls

--- src/main/java/com/petadoption/center/dto/breed/BreedUpdateDto.java
+++ src/main/java/com/petadoption/center/dto/breed/BreedUpdateDto.java
@@ -7,6 +7,6 @@ import static com.petadoption.center.util.Messages.ONLY_LETTERS;
 
 @Builder
 public record BreedUpdateDto(
-        @Pattern(regexp = "[a-zA-Z]+", message = ONLY_LETTERS)
+        @Pattern(regexp = "[a-zA-Z ]+", message = ONLY_LETTERS)
         String name
 ) {}
