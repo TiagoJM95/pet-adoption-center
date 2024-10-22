376545d816949f3fe3d79170e49a63034e02325c
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-10-22 15:43:46 +0100

    refactored adoptionFormController integration tests

--- src/test/java/com/petadoption/center/controller/integration/AdoptionFormControllerTest.java
+++ src/test/java/com/petadoption/center/controller/integration/AdoptionFormControllerTest.java
@@ -3,82 +3,98 @@ package com.petadoption.center.controller.integration;
 import com.petadoption.center.dto.adoptionForm.AdoptionFormCreateDto;
 import com.petadoption.center.dto.adoptionForm.AdoptionFormGetDto;
 import com.petadoption.center.dto.adoptionForm.AdoptionFormUpdateDto;
+import com.petadoption.center.dto.breed.BreedGetDto;
+import com.petadoption.center.dto.color.ColorGetDto;
+import com.petadoption.center.dto.organization.OrganizationGetDto;
 import com.petadoption.center.dto.pet.PetCreateDto;
 import com.petadoption.center.dto.pet.PetGetDto;
-import com.petadoption.center.dto.user.UserCreateDto;
+import com.petadoption.center.dto.species.SpeciesGetDto;
 import com.petadoption.center.dto.user.UserGetDto;
-import com.petadoption.center.model.embeddable.Address;
-import com.petadoption.center.model.embeddable.Family;
+
 import org.junit.jupiter.api.*;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.List;
+import java.util.Optional;
 
-import static com.petadoption.center.testUtils.TestDtoFactory.userCreateDto;
-import static com.petadoption.center.testUtils.TestEntityFactory.createAttributes;
+import static com.petadoption.center.testUtils.ConstantsURL.*;
+import static com.petadoption.center.testUtils.TestDtoFactory.*;
+import static com.petadoption.center.testUtils.TestEntityFactory.*;
 import static com.petadoption.center.util.Messages.ADOPTION_FORM_DELETE_MESSAGE;
 import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class AdoptionFormControllerTest extends AbstractIntegrationTest {
 
-    private AdoptionFormCreateDto adoptionFormCreateDto;
-    private AdoptionFormGetDto adoptionFormGetDto;
-    private AdoptionFormUpdateDto adoptionFormUpdateDto;
+    private static AdoptionFormCreateDto adoptionFormCreateDto;
+    private static AdoptionFormUpdateDto adoptionFormUpdateDto;
+    private static AdoptionFormGetDto expectedAdoptionFormGetDto;
+
+
+    private SpeciesGetDto speciesGetDto;
+    private BreedGetDto primaryBreedGetDto;
+    private ColorGetDto primaryColorGetDto;
+    private OrganizationGetDto organizationGetDto;
+    private PetCreateDto petCreateDto;
     private UserGetDto userGetDto;
     private PetGetDto petGetDto;
-    private String userId;
-    private String petId;
-    private String orgId;
-    private String speciesId;
-    private String breedId;
-    private String colorId;
-    private String adoptionFormId;
 
+    @BeforeAll
+    void setup() throws Exception {
+        speciesGetDto = persistSpecies(speciesCreateDto());
+        primaryBreedGetDto = persistBreed(primaryBreedCreateDto(speciesGetDto.id()));
+        primaryColorGetDto = persistColor(primaryColorCreateDto());
+        organizationGetDto = persistOrganization(organizationCreateDto());
 
-    @BeforeEach
-    void setUp() throws Exception {
-        speciesId = helper.persistTestSpecies();
-        breedId = helper.persistTestPrimaryBreed();
-        colorId = helper.persistTestPrimaryColor();
-        orgId = helper.persistTestOrg();
+        petCreateDto = PetCreateDto.builder()
+                .name("Max")
+                .speciesId(speciesGetDto.id())
+                .primaryBreedId(primaryBreedGetDto.id())
+                .primaryColor(primaryColorGetDto.id())
+                .gender("Male")
+                .coat("Medium")
+                .age("Adult")
+                .size("Large")
+                .description("Max is friendly dog!")
+                .imageUrl("https://dog.com")
+                .isAdopted(false)
+                .attributes(createAttributes())
+                .organizationId(organizationGetDto.id())
+                .build();
 
-        Family family = new Family(
-                4,
-                true,
-                true,
-                2,
-                List.of("DOG", "PARROT")
-        );
+        userGetDto = persistUser(userCreateDto());
+        petGetDto = persistPet(petCreateDto);
 
-        Address address = new Address("Rua das Andorinhas, 123",
-                "Vila Nova de Gaia",
-                "Porto",
-                "4410-000");
+        adoptionFormCreateDto = AdoptionFormCreateDto.builder()
+                .userId(userGetDto.id())
+                .petId(petGetDto.id())
+                .userFamily(createFamily())
+                .petVacationHome("Pet Hotel")
+                .isResponsibleForPet(true)
+                .otherNotes("Notes")
+                .petAddress(createAddress())
+                .build();
 
-        addUser();
-        addPet();
+        adoptionFormUpdateDto = AdoptionFormUpdateDto.builder()
+                .userFamily(createFamily())
+                .petVacationHome("Neighbour")
+                .isResponsibleForPet(true)
+                .otherNotes("Notes")
+                .petAddress(createAddress())
+                .build();
 
-        adoptionFormCreateDto = new AdoptionFormCreateDto(
-                userId,
-                petId,
-                family,
-                "Neighbour",
-                true,
-                "Notes",
-                address
-        );
-
-        adoptionFormUpdateDto = new AdoptionFormUpdateDto(
-                family,
-                "Pet Hotel",
-                false,
-                "Only 2 weeks per year of vacation",
-                address
-        );
+        expectedAdoptionFormGetDto = AdoptionFormGetDto.builder()
+                .user(userGetDto)
+                .pet(petGetDto)
+                .userFamily(createFamily())
+                .petVacationHome("Pet Hotel")
+                .isResponsibleForPet(true)
+                .otherNotes("Notes")
+                .petAddress(createAddress())
+                .build();
     }
 
     @AfterEach
@@ -87,77 +103,11 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
         clearRedisCache();
     }
 
-    private void addUser() throws Exception {
-        UserCreateDto userCreateDto = userCreateDto();
-
-        MvcResult result = mockMvc.perform(post("/api/v1/user/")
-                .content(objectMapper.writeValueAsString(userCreateDto))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-        userGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), UserGetDto.class);
-
-        userId = userGetDto.id();
-    }
-
-    private void addPet() throws Exception {
-        PetCreateDto petCreateDto = PetCreateDto.builder()
-                .name("Max")
-                .speciesId(speciesId)
-                .primaryBreedId(breedId)
-                .primaryColor(colorId)
-                .gender("MALE")
-                .coat("SHORT")
-                .size("SMALL")
-                .age("ADULT")
-                .description("Max is a very friendly dog")
-                .imageUrl("https://www.dogimages.com")
-                .isAdopted(false)
-                .attributes(createAttributes())
-                .organizationId(orgId)
-                .build();
-
-        MvcResult result = mockMvc.perform(post("/api/v1/pet/addSingle")
-                        .content(objectMapper.writeValueAsString(petCreateDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-
-        petGetDto = objectMapper.readValue(result.getResponse().getContentAsString(), PetGetDto.class);
-
-        petId = petGetDto.id();
-    }
-
-    private void addAdoptionForm() throws Exception{
-
-        MvcResult result = mockMvc.perform(post("/api/v1/adoption-form/")
-                        .content(objectMapper.writeValueAsString(adoptionFormCreateDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andReturn();
-
-        AdoptionFormGetDto adoptionFormCreated = objectMapper.readValue(result.getResponse().getContentAsString(), AdoptionFormGetDto.class);
-
-        adoptionFormId = adoptionFormCreated.id();
-
-        adoptionFormGetDto = new AdoptionFormGetDto(
-                adoptionFormId,
-                userGetDto,
-                petGetDto,
-                adoptionFormCreated.userFamily(),
-                adoptionFormCreated.petVacationHome(),
-                adoptionFormCreated.isResponsibleForPet(),
-                adoptionFormCreated.otherNotes(),
-                adoptionFormCreated.petAddress(),
-                adoptionFormCreated.createdAt()
-        );
-    }
-
     @Test
     @DisplayName("Test get all adoption forms when empty returns empty")
     void testGetAllEmptyAdoptionFormsReturnsEmpty() throws Exception {
 
-        mockMvc.perform(get("/api/v1/adoption-form/")
+        mockMvc.perform(get(ADOPTION_FORM_GET_ALL_OR_CREATE_URL)
                 .param("page", "0")
                 .param("size", "5")
                 .param("sort", "id")
@@ -172,18 +122,18 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     @DisplayName("Test get all adoption with 1 element returns 1 element")
     void testGetAllReturnsOne() throws Exception {
 
-        addAdoptionForm();
+        AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
 
-        mockMvc.perform(get("/api/v1/adoption-form/")
+        mockMvc.perform(get(ADOPTION_FORM_GET_ALL_OR_CREATE_URL)
                         .param("page", "0")
                         .param("size", "5")
                         .param("sort", "id")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(1)))
-                .andExpect(jsonPath("$[0].id", is(adoptionFormGetDto.id())))
-                .andExpect(jsonPath("$[0].user.id", is(adoptionFormGetDto.user().id())))
-                .andExpect(jsonPath("$[0].pet.id", is(adoptionFormGetDto.pet().id())))
+                .andExpect(jsonPath("$[0].id", is(persistedAdoptionForm.id())))
+                .andExpect(jsonPath("$[0].user.id", is(persistedAdoptionForm.user().id())))
+                .andExpect(jsonPath("$[0].pet.id", is(persistedAdoptionForm.pet().id())))
                 .andReturn();
     }
 
@@ -191,55 +141,47 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     @DisplayName("Test if create adoption form works correctly")
     void testCreateAdoptionForm() throws Exception {
 
-        MvcResult result = mockMvc.perform(post("/api/v1/adoption-form/")
-                        .content(objectMapper.writeValueAsString(adoptionFormCreateDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andReturn();
+        AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
 
-        AdoptionFormGetDto adoptionFormCreated = objectMapper.readValue(result.getResponse().getContentAsString(), AdoptionFormGetDto.class);
+        assertThat(persistedAdoptionForm)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(expectedAdoptionFormGetDto);
 
-        adoptionFormId = adoptionFormCreated.id();
-
-        adoptionFormGetDto = new AdoptionFormGetDto(
-                adoptionFormId,
-                userGetDto,
-                petGetDto,
-                adoptionFormCreated.userFamily(),
-                adoptionFormCreated.petVacationHome(),
-                adoptionFormCreated.isResponsibleForPet(),
-                adoptionFormCreated.otherNotes(),
-                adoptionFormCreated.petAddress(),
-                adoptionFormCreated.createdAt()
-        );
+        assertNotNull(persistedAdoptionForm.createdAt());
+        assertTrue(persistedAdoptionForm.id().matches("^[0-9a-fA-F-]{36}$"));
     }
 
     @Test
     @DisplayName("Test if update an adoption form works correctly")
     void testUpdate() throws Exception {
 
-        addAdoptionForm();
+        AdoptionFormGetDto adoptionFormGetDto = persistAdoptionForm(adoptionFormCreateDto);
+        String adoptionFormId = adoptionFormGetDto.id();
 
-        mockMvc.perform(put("/api/v1/adoption-form/update/{id}", adoptionFormId)
+        mockMvc.perform(put(ADOPTION_FORM_UPDATE_URL, adoptionFormId)
                         .content(objectMapper.writeValueAsString(adoptionFormUpdateDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.petVacationHome", is(adoptionFormUpdateDto.petVacationHome())))
                 .andExpect(jsonPath("$.isResponsibleForPet", is(adoptionFormUpdateDto.isResponsibleForPet())))
                 .andExpect(jsonPath("$.otherNotes", is(adoptionFormUpdateDto.otherNotes())));
-
-
     }
 
     @Test
     @DisplayName("Test if delete an adoption form works correctly")
     void testDelete() throws Exception {
 
-        testUpdate();
+        AdoptionFormGetDto adoptionFormGetDto = persistAdoptionForm(adoptionFormCreateDto);
+        String adoptionFormId = adoptionFormGetDto.id();
 
-        mockMvc.perform(delete("/api/v1/adoption-form/delete/{id}", adoptionFormId)
+        mockMvc.perform(delete(ADOPTION_FORM_DELETE_URL, adoptionFormId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(format(ADOPTION_FORM_DELETE_MESSAGE, adoptionFormId)));
+
+        assertEquals(Optional.empty(), adoptionFormRepository.findById(adoptionFormId));
+
     }
 }
