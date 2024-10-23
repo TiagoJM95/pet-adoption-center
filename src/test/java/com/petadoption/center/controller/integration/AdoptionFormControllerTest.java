7166a4ef1df34a45e91539d8d89eeb2b5b7db79c
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-10-23 15:28:19 +0100

    added tests to AdoptionForm integration controller

--- src/test/java/com/petadoption/center/controller/integration/AdoptionFormControllerTest.java
+++ src/test/java/com/petadoption/center/controller/integration/AdoptionFormControllerTest.java
@@ -13,13 +13,14 @@ import com.petadoption.center.dto.user.UserGetDto;
 
 import org.junit.jupiter.api.*;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Optional;
 
 import static com.petadoption.center.testUtils.ConstantsURL.*;
 import static com.petadoption.center.testUtils.TestDtoFactory.*;
 import static com.petadoption.center.testUtils.TestEntityFactory.*;
-import static com.petadoption.center.util.Messages.ADOPTION_FORM_DELETE_MESSAGE;
+import static com.petadoption.center.util.Messages.*;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -29,12 +30,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class AdoptionFormControllerTest extends AbstractIntegrationTest {
 
+
     private static AdoptionFormCreateDto adoptionFormCreateDto;
     private static AdoptionFormUpdateDto adoptionFormUpdateDto;
     private static AdoptionFormGetDto expectedAdoptionFormGetDto;
 
+    private String invalidAdoptionFormId;
+
+    private String UNIQUE_USER_PET_CONSTRAINT;
+
     @BeforeAll
     void setUp() throws Exception {
+        UNIQUE_USER_PET_CONSTRAINT = "uniqueuserandpet";
+
         SpeciesGetDto speciesGetDto = persistSpecies(speciesCreateDto());
         BreedGetDto primaryBreedGetDto = persistBreed(primaryBreedCreateDto(speciesGetDto.id()));
         ColorGetDto primaryColorGetDto = persistColor(primaryColorCreateDto());
@@ -86,6 +94,8 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
                 .otherNotes("Notes")
                 .petAddress(createAddress())
                 .build();
+
+        invalidAdoptionFormId = "1111-1111";
     }
 
     @AfterEach
@@ -95,8 +105,8 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test get all adoption forms when empty returns empty")
-    void testGetAllEmptyAdoptionFormsReturnsEmpty() throws Exception {
+    @DisplayName("Test if get all AdoptionForms when repository is empty returns empty list")
+    void testIfGetAllEmptyAdoptionFormsReturnsEmptyList() throws Exception {
 
         mockMvc.perform(get(ADOPTION_FORM_GET_ALL_OR_CREATE_URL)
                 .param("page", "0")
@@ -110,8 +120,8 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test get all adoption with 1 element returns 1 element")
-    void testGetAllReturnsOne() throws Exception {
+    @DisplayName("Test if getting all AdoptionForms with 1 element in repository returns 1 element list")
+    void testIfGetAllAdoptionFormWhenRepositorsHasOneElementReturnsListOfSizeOne() throws Exception {
 
         AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
 
@@ -129,8 +139,38 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test if create adoption form works correctly")
-    void testCreateAdoptionForm() throws Exception {
+    @DisplayName("Test if getting an adoption form with a valid id returns the correct entry")
+    void testIfGetAdoptionFormWithValidIdReturnsEntry() throws Exception {
+
+        AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
+
+        MvcResult result = mockMvc.perform(get(ADOPTION_FORM_GET_BY_ID_URL, persistedAdoptionForm.id())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        AdoptionFormGetDto retrievedAdoptionForm = objectMapper.readValue(result.getResponse().getContentAsString(), AdoptionFormGetDto.class);
+
+        assertThat(persistedAdoptionForm)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .ignoringFieldsMatchingRegexes(".*createdAt")
+                .isEqualTo(retrievedAdoptionForm);
+    }
+
+    @Test
+    @DisplayName("Test if getting an adoption form with an invalid id returns an AdoptionNotFoundException with message")
+    void testIfGetAdoptionFormWithInvalidIdReturnsExceptionMessage() throws Exception {
+
+        mockMvc.perform(get(ADOPTION_FORM_GET_BY_ID_URL, invalidAdoptionFormId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(format(ADOPTION_FORM_NOT_FOUND, invalidAdoptionFormId)));
+    }
+
+    @Test
+    @DisplayName("Test if create adoption persists an AdoptionForm and returns the correct AdoptionFormGetDto")
+    void testIfCreateAdoptionFormPersistsAndReturnsDto() throws Exception {
 
         AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
 
@@ -145,8 +185,25 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test if update an adoption form works correctly")
-    void testUpdate() throws Exception {
+    @DisplayName("Test if create adoption with repeated User/Pet combination returns conflict with correct constraint and message")
+    void testIfCreateAdoptionWithExistentUserAndPetReturnsConflict() throws Exception {
+
+        AdoptionFormGetDto persistedAdoptionForm = persistAdoptionForm(adoptionFormCreateDto);
+
+        assertNotNull(persistedAdoptionForm.createdAt());
+        assertTrue(persistedAdoptionForm.id().matches("^[0-9a-fA-F-]{36}$"));
+
+        mockMvc.perform(post(ADOPTION_FORM_GET_ALL_OR_CREATE_URL)
+                        .content(objectMapper.writeValueAsString(adoptionFormCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value(constraintMessageResolver.getMessage(UNIQUE_USER_PET_CONSTRAINT)))
+                .andExpect(jsonPath("$.constraint").value(UNIQUE_USER_PET_CONSTRAINT));
+    }
+
+    @Test
+    @DisplayName("Test if update an AdoptionForm correctly updates the fields with an AdoptionFormUpdateDto")
+    void testIfUpdateAdoptionFormUpdatesFieldsWithSentDto() throws Exception {
 
         AdoptionFormGetDto adoptionFormGetDto = persistAdoptionForm(adoptionFormCreateDto);
         String adoptionFormId = adoptionFormGetDto.id();
@@ -161,8 +218,8 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test if delete an adoption form works correctly")
-    void testDelete() throws Exception {
+    @DisplayName("Test if delete an AdoptionForm correctly removes the entry from the repository")
+    void testIfDeleteAdoptionFormRemovesEntryFromRepository() throws Exception {
 
         AdoptionFormGetDto adoptionFormGetDto = persistAdoptionForm(adoptionFormCreateDto);
         String adoptionFormId = adoptionFormGetDto.id();
@@ -174,5 +231,15 @@ public class AdoptionFormControllerTest extends AbstractIntegrationTest {
 
         assertEquals(Optional.empty(), adoptionFormRepository.findById(adoptionFormId));
 
+    }
+
+    @Test
+    @DisplayName("Test if deleting an AdoptionForm with an invalid id returns an AdoptionNotFoundException with message")
+    void testIfDeleteAdoptionFormWithInvalidIdReturnsExceptionMessage() throws Exception {
+
+        mockMvc.perform(delete(ADOPTION_FORM_DELETE_URL, invalidAdoptionFormId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(format(ADOPTION_FORM_NOT_FOUND, invalidAdoptionFormId)));
     }
 }
